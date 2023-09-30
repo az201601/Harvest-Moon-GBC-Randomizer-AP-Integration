2732aa67f95cf3db7d9c3697cc5727f384c53349
az201601 <godblessus21@gmail.com>
2023-09-30 23:38:35 +1300

    Add files via upload

--- hmRand.java
+++ hmRand.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Random;
 
 public class hmRand {
-	
-	public static boolean randMoney = false;
-	
+
+    public static boolean randMoney = false;
+
     public static void main(String[] args) {
         // Create a JFrame (window)
         JFrame frame = new JFrame("Harvest Moon GBC Randomizer");
@@ -24,17 +27,14 @@ public class hmRand {
     private static void placeComponents(JPanel panel) {
         panel.setLayout(null);
 
-        // Create a label
         JLabel romLabel = new JLabel("Select ROM:");
         romLabel.setBounds(10, 20, 80, 25);
         panel.add(romLabel);
 
-        // Create a label to display the ROM path
         JTextField romField = new JTextField();
         romField.setBounds(100, 20, 150, 25);
         panel.add(romField);
 
-        // Create a button for browsing for the ROM
         JButton browseButton = new JButton("Browse");
         browseButton.setBounds(10, 50, 80, 25);
         browseButton.addActionListener(new ActionListener() {
@@ -49,74 +49,56 @@ public class hmRand {
             }
         });
         panel.add(browseButton);
-		
-		JCheckBox rmoney = new JCheckBox("Randomize the amount of Gold you start with");
-		rmoney.setBounds(10, 80, 200, 25);
-		panel.add(rmoney);
-		rmoney.addItemListener(new ItemListener()
-		{
-			public void itemStateChanged(ItemEvent ie)
-			{
-				randMoney = ie.getStateChange() == ItemEvent.SELECTED;
-			}
-		});
-		
-		JButton randomizeButton = new JButton("Randomize!");
-		randomizeButton.setBounds(10, 110, 100, 25);
-		randomizeButton.addActionListener(new actionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent ae)
-			{
-				String romPath = romField.getText();
-				File rom = new File(rompath);
-				randomize(rom);
-			}
-		});
-		panel.add(randomizerButton);
+
+        JCheckBox rmoney = new JCheckBox("Randomize the amount of Gold you start with");
+        rmoney.setBounds(10, 80, 200, 25);
+        panel.add(rmoney);
+        rmoney.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent ie) {
+                randMoney = ie.getStateChange() == ItemEvent.SELECTED;
+            }
+        });
+
+        JButton randomizeButton = new JButton("Randomize!");
+        randomizeButton.setBounds(10, 110, 100, 25);
+        randomizeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                String romPath = romField.getText();
+                File rom = new File(romPath);
+                randomize(rom);
+            }
+        });
+        panel.add(randomizeButton);
     }
-	
-	public static void randomize(File file)
-	{
-		if(randMoney)
-		{
-			try(RandomAccessFile rom = new RandomAccessFile(file, "rw")
-			{
-				rom.seek(0xB8EF);
-				int currentValue = (rom.read() << 16) | (rom.read() << 8) | rom.read();
-				Random random = new Random();
+
+    public static void randomize(File file) {
+        if (randMoney) {
+            try (RandomAccessFile rom = new RandomAccessFile(file, "rw")) {
+                rom.seek(0xB8EF);
+                int currentValue = (rom.read() << 16) | (rom.read() << 8) | rom.read();
+                Random random = new Random();
                 int randomGP = random.nextInt(0x1F5);
-				int newValue = (currentValue & 0xFF0000) | randomGP;
-				rom.seek(0xB8EF);
+                int newValue = (currentValue & 0xFF0000) | randomGP;
+                rom.seek(0xB8EF);
                 rom.write((newValue >> 16) & 0xFF);
                 rom.write((newValue >> 8) & 0xFF);
                 rom.write(newValue & 0xFF);
-			}
-			catch (IOException e) 
-			{
-                e.printStackTrace();
-            }
-			
-			  String randomizerDir = System.getProperty("user.dir");
-			  String randomizedRomPath = randomizerDir + File.separator + "randomized_rom.gbc";
-              File randomizedRomFile = new File(randomizedRomPath);
-			  try (RandomAccessFile randomizedRom = new RandomAccessFile(randomizedRomFile, "rw")) 
-			  {
-                    // Copy the content from original ROM to randomized ROM
+
+                String randomizerDir = System.getProperty("user.dir");
+                String randomizedRomPath = randomizerDir + File.separator + "randomized_rom.gbc";
+                File randomizedRomFile = new File(randomizedRomPath);
+                try (RandomAccessFile randomizedRom = new RandomAccessFile(randomizedRomFile, "rw")) {
                     byte[] buffer = new byte[1024];
                     int bytesRead;
-                    while ((bytesRead = rom.read(buffer)) != -1) 
-					{
+                    while ((bytesRead = rom.read(buffer)) != -1) {
                         randomizedRom.write(buffer, 0, bytesRead);
-					}
-			 rom.close();
-             randomizedRom.close();
-			 }
-			 catch (IOException e) 
-			 {
-				 e.printStackTrace();
-			 }
-		}
-	}        
-				
+                    }
+                }
+                rom.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
